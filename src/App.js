523aae71d3bc3065b6f748c56aed1f3776f3580c
YauheniButski <ybutski@gmail.com>
2019-04-04 17:59:45 +0300

    1. Wrap App into React-Redux Provider. 2. Create a store for our chat. 3.  Add actions. 4. Add action's types as constants. 5. Add reducers.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/* import { Sidebar } from "./containers/Sidebar";
+import { MessageList } from "./containers/MessageList";
+import { AddMessage } from "./containers/AddMessage"; */
+
 class App extends Component {
   render() {
     return (
       <div id="container">
         <aside id="sidebar">Users</aside>
+        {/* <Sidebar /> */}
         <section id="main">
           <section id="message-list">Message List</section>
+          {/* <MessageList /> */}
           <section id="new-message">New Message</section>
+          {/* <AddMessage /> */}
         </section>
       </div>
     );
---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+import * as types from '../constants/ActionTypes';
+
+let nextMessageId = 0;
+const nextUserId = 0;
+
+//you send new message
+export const addMessage = (message, author) => ({
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
+});
+
+export const addUser = name => ({
+    type: types.ADD_USER,
+    id: nextUserId++,
+    name
+});
+
+//you receive message from someone else
+export const messageReceived = (message, author) => ({
+    type: types.MESSAGE_RECEIVED,
+    id: nextMessageId++,
+    message,
+    author
+});
+
+export const populateusersList = users => ({
+    type: types.USER_LIST,
+    users
+});
