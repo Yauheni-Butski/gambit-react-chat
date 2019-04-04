40ee0ebac75a79f17bfbd574ec90301da99821dc
YauheniButski <ybutski@gmail.com>
2019-04-04 17:11:20 +0300

    Add static html+css content for beginning.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
+      <div id="container">
+        <aside id="sidebar">Users</aside>
+        <section id="main">
+          <section id="message-list">Message List</section>
+          <section id="new-message">New Message</section>
+        </section>
       </div>
     );
   }
