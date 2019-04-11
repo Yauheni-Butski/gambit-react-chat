74fd3bdfb2c8c51499badf1349e08275de64ac9b
YauheniButski <ybutski@gmail.com>
2019-04-11 12:03:10 +0300

    Delete old component

--- src/containers/Sidebar.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import SidebarComponent from '../components/Sidebar'
-
-const mapStateToProps = state => ({
-    users: state.usersState
-});
-
-export const Sidebar = connect(mapStateToProps)(SidebarComponent)
