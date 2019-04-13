fbc1672a116a0f383c547e12dfccf503d4cea9e2
ybutski <ybutski@gmail.com>
2019-04-13 16:50:25 +0300

    1. Move functions for rooms in RoomSaga. 2. ChatSaga now composing all needed sagas.

--- src/sagas/room.js
+++ src/sagas/room.js
@@ -1,13 +1,89 @@
-import { takeEvery, select, put } from 'redux-saga/effects';
+import { takeEvery, select, put, call } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import * as types from '../constants/ActionTypes';
+import { messageReceived, clearMessages, updateRoomList, updateCurrentRoom } from '../actions/index';
 
-function* updateCurrentRoom(action){
-    const currentUser = yield select(state => state.currentRoomState);
-    /* логика обработки */
-}
+function enterToRoomChannel(currentUser, action){
+    return eventChannel(emit => {
+  
+      const onMessageHandler = (message) => {
+        message.parts.forEach(messagePart => {
+          //TODO. 3. Всё передаём в Reducer или в State. View логика уже должна будет сама решать как что отрисовывать
+          //TODO. Обрабатывать 3 типа сообщения
+          //TODO. https://docs.pusher.com/chatkit/reference/javascript#messages
+          let text = '';
+          switch(messagePart.partType){
+            case "url":
+              text = messagePart.payload.url;
+              break;
+            case "attachment":
+              // temporary send only name
+              text = messagePart.payload.name;
+              break;
+            case "inline":
+            default:
+              text = messagePart.payload.content;
+              break;
+          }
+          emit(messageReceived(message.senderId, text));
+        });
+      };
+  
+      currentUser.subscribeToRoomMultipart({
+        roomId: action.roomId,
+        messageLimit: 20,
+        hooks: {
+          onMessage: onMessageHandler
+        }
+      })
+      .then(room => {
+        emit(updateCurrentRoom(room));
+        //after we subscribe to room, our collections 'joinableRooms' and 'joinedRooms' will changed
+        //one room from 'joinable' goes to 'joined'. So we should update our state
+        currentUser.getJoinableRooms()
+        .then(joinableRooms => {
+          let joinedRooms = currentUser.rooms;
+          emit(updateRoomList(joinableRooms, joinedRooms))
+        })
+        .catch(err => console.log('error on joinableRooms: ', err));
+      });
+  
+  
+      const unsubscribe = () => {
+        currentUser.roomSubscriptions[action.roomId].cancel();
+      }
+  
+      return unsubscribe;
+    }); 
+  }
+
+  function* onEnterRoomChannelEmit(action){
+    switch(action.type){
+      case types.MESSAGE_RECEIVED:
+        yield put(messageReceived(action.senderId, action.text));
+        break;
+      case types.UPD_CURR_ROOM:
+        yield put(updateCurrentRoom(action.currentRoom));
+        break;
+      case types.UPD_ROOM_LIST:
+        yield put(updateRoomList(action.joinableRooms, action.joinedRooms));
+        break;
+      default:
+        break;
+    }
+  }
+
+  function* enterToRoom(action){
+    const currentUser = yield select(state => state.currentUserState);
+    //1. clear messages in current MessageList
+    yield put(clearMessages());
+
+    const chan = yield call(enterToRoomChannel, currentUser, action);
+    yield takeEvery(chan, onEnterRoomChannelEmit);
+  }
 
 const roomSaga = function* () {
-    yield takeEvery(types.UPD_CURR_ROOM, updateCurrentRoom)
+    yield takeEvery(types.ENTER_TO_ROOM, enterToRoom)
 }
 
 export default roomSaga;
