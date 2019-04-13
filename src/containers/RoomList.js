525b0d8917696974b4afabd6bc69d75f8889a2db
ybutski <ybutski@gmail.com>
2019-04-13 16:41:43 +0300

    1. Reduce size of RoomList container. Moved all logic to EnterToRoom in Saga. 2. Create room saga finctionality for EnterToRoom action. TEMP stored in index.js

--- src/containers/RoomList.js
+++ src/containers/RoomList.js
@@ -1,71 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { enterToRoom/* , messageReceived, clearMessages, updateRoomList, updateCurrentRoom */ } from '../actions/index';
+import { enterToRoom } from '../actions/index';
 import RoomListComponent from '../components/RoomList'
 
 class RoomList extends Component{
-    constructor(){
-        super()
-        this.subscribeToRoom = this.subscribeToRoom.bind(this);
-    }
-
-    subscribeToRoom(roomId){
-
-      this.props.enterToRoom(roomId);
-
-      /* TODO. Одно и тоже действие в NewRoomForm и RoomList. Как можно улучшить? */
-      //clear messages state, for new room
-      /* this.props.clearMessages();
-
-      let currentUser = this.props.currentUser;
-      currentUser.subscribeToRoomMultipart({
-        roomId: roomId,
-        messageLimit: 20,
-        hooks: {
-          onMessage: message => {
-            message.parts.forEach(messagePart => {
-              //TODO. 3. Всё передаём в Reducer или в State. View логика уже должна будет сама решать как что отрисовывать
-              //TODO. Обрабатывать 3 типа сообщения
-              //TODO. https://docs.pusher.com/chatkit/reference/javascript#messages
-              let text = '';
-              switch(messagePart.partType){
-                case "url":
-                  text = messagePart.payload.url;
-                  break;
-                case "attachment":
-                  // temporary send only name
-                  text = messagePart.payload.name;
-                  break;
-                case "inline":
-                default:
-                  text = messagePart.payload.content;
-                  break;
-              }
-
-              this.props.onMessageReceived(message.senderId, text);
-            });
-          }
-        }
-      })
-      .then(room => {
-          this.props.updateCurrentRoom(room);
-
-          //after we subscribe to room, our collections 'joinableRooms' and 'joinedRooms' will changed
-          //one room from 'joinable' goes to 'joined'. So we should update our state
-          currentUser.getJoinableRooms()
-          .then(joinableRooms => {
-            let joinedRooms = currentUser.rooms;
-            this.props.populateRooms(joinableRooms, joinedRooms);
-          })
-          .catch(err => console.log('error on joinableRooms: ', err));
-      })
-      .catch(err => console.log('error on subscribing to room: ', err)); */
-    }
 
     render(){
         return (
             <RoomListComponent 
-                subscribeToRoom={this.subscribeToRoom}
+                subscribeToRoom={this.props.enterToRoom}
                 rooms={this.props.rooms}
                 currentRoomId={this.props.currentRoomId}/>
         )
@@ -74,26 +17,13 @@ class RoomList extends Component{
 
 const mapStateToProps = state => ({
     rooms: state.roomsState,
-   /*  currentUser: state.currentUserState, */
     currentRoomId: state.currentRoomState.id
 });
 
 const mapDispatchToProps = dispatch => ({
     enterToRoom: roomId => {
       dispatch(enterToRoom(roomId))
-    },
-/*     onMessageReceived: (senderId, text) => {
-       dispatch(messageReceived(senderId, text));
-    }, */
-/*     clearMessages: () => {
-        dispatch(clearMessages());
-    }, */
-/*     populateRooms: (joinableRooms, joinedRooms) => {
-        dispatch(updateRoomList(joinableRooms, joinedRooms));
-    }, */
-/*     updateCurrentRoom: (currentRoom) => {
-        dispatch(updateCurrentRoom(currentRoom));
-    } */
+    }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RoomList)
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,65 @@
-import { takeEvery, select, put } from 'redux-saga/effects';
+import { takeEvery, select, put, call } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import * as types from '../constants/ActionTypes';
 import { messageReceived, clearMessages, updateRoomList, updateCurrentRoom } from '../actions/index';
 //import roomSaga from './room'
 
+function enterToRoomChannel(currentUser, action){
+  return eventChannel(emit => {
 
-function* enterToRoom(action){
+    const onMessageHandler = (message) => {
+      message.parts.forEach(messagePart => {
+        //TODO. 3. Всё передаём в Reducer или в State. View логика уже должна будет сама решать как что отрисовывать
+        //TODO. Обрабатывать 3 типа сообщения
+        //TODO. https://docs.pusher.com/chatkit/reference/javascript#messages
+        let text = '';
+        switch(messagePart.partType){
+          case "url":
+            text = messagePart.payload.url;
+            break;
+          case "attachment":
+            // temporary send only name
+            text = messagePart.payload.name;
+            break;
+          case "inline":
+          default:
+            text = messagePart.payload.content;
+            break;
+        }
+        emit(messageReceived(message.senderId, text));
+      });
+    };
+
+    currentUser.subscribeToRoomMultipart({
+      roomId: action.roomId,
+      messageLimit: 20,
+      hooks: {
+        onMessage: onMessageHandler
+      }
+    })
+    .then(room => {
+      emit(updateCurrentRoom(room));
+      //after we subscribe to room, our collections 'joinableRooms' and 'joinedRooms' will changed
+      //one room from 'joinable' goes to 'joined'. So we should update our state
+      currentUser.getJoinableRooms()
+      .then(joinableRooms => {
+        let joinedRooms = currentUser.rooms;
+        emit(updateRoomList(joinableRooms, joinedRooms))
+      })
+      .catch(err => console.log('error on joinableRooms: ', err));
+    });
+
+
+    const unsubscribe = () => {
+      currentUser.roomSubscriptions[action.roomId].cancel();
+    }
+
+    return unsubscribe;
+  }); 
+}
+
+
+/* function* enterToRoom(action){
     const currentUser = yield select(state => state.currentUserState);
     //1. clear messages in current MessageList
     yield put(clearMessages());
@@ -34,28 +89,52 @@ function* enterToRoom(action){
                   break;
               }
               console.log('Message received');
-              /* TODO. Channel */put(messageReceived(message.senderId, text));
+              put(messageReceived(message.senderId, text)); // TODO. Channel
             });
           }
         }
       })
       //3. Update room list
       .then(room => {
-        /* TODO. Channel */put(updateCurrentRoom(room));
+        put(updateCurrentRoom(room));// TODO. Channel
         console.log('Saga. Then. UpdateCurrentRoom');
         //after we subscribe to room, our collections 'joinableRooms' and 'joinedRooms' will changed
         //one room from 'joinable' goes to 'joined'. So we should update our state
         currentUser.getJoinableRooms()
         .then(joinableRooms => {
           let joinedRooms = currentUser.rooms;
-          /* TODO. Channel */put(updateRoomList(joinableRooms, joinedRooms))
+          put(updateRoomList(joinableRooms, joinedRooms))// TODO. Channel
         })
         .catch(err => console.log('error on joinableRooms: ', err));
     })
+} */
+
+function* onEnterRoomChannelEmit(action){
+  switch(action.type){
+    case types.MESSAGE_RECEIVED:
+      yield put(messageReceived(action.senderId, action.text));
+      break;
+    case types.UPD_CURR_ROOM:
+      yield put(updateCurrentRoom(action.currentRoom));
+      break;
+    case types.UPD_ROOM_LIST:
+      yield put(updateRoomList(action.joinableRooms, action.joinedRooms));
+      break;
+  }
 }
 
+function* enterToRoom(action){
+  const currentUser = yield select(state => state.currentUserState);
+  //1. clear messages in current MessageList
+  yield put(clearMessages());
+
+  const chan = yield call(enterToRoomChannel, currentUser, action);
+  yield takeEvery(chan, onEnterRoomChannelEmit);
+}
+
+
 const chatSaga = function* () {
-    yield takeEvery(types.ENTER_TO_ROOM, enterToRoom)
+  yield takeEvery(types.ENTER_TO_ROOM, enterToRoom)
 }
 
 export default chatSaga;
